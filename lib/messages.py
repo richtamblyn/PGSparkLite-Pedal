e8eccf51f32e7e39b23d205b16d99c7f551384bc
Rich Tamblyn <richard.tamblyn@gmail.com>
2021-03-07 11:09:16 +0000

    Fix font bug

--- lib/messages.py
+++ lib/messages.py
@@ -1,5 +1,5 @@
-msg_is_amp_on = 'Is Amp on? \r\nAttempt: '
+msg_is_amp_on = 'Is Amp on? \nAttempt: '
 msg_no_connection = 'No connection'
 msg_disconnected = 'Disconnected'
 msg_booting = 'Booting...'
-msg_pgsparklite_ok = 'PGSparkLite\r\nVersion 1.0\r\n07/03/2021'
+msg_pgsparklite_ok = 'PGSparkLite\nVersion 1.0\n07/03/2021'
