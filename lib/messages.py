eeced0f17b11c542ce28077112c3b34718c21146
RichT <richard.tamblyn@gmail.com>
2021-03-28 16:10:02 +0100

    Messages fixed for HD44780

--- lib/messages.py
+++ lib/messages.py
@@ -1,7 +1,11 @@
-msg_is_amp_on = 'Is Amp on? \nAttempt: '
+##################
+# Common messages
+##################
+
+msg_is_amp_on = 'Is Amp on? \n\rAttempt: '
 msg_no_connection = 'No connection'
 msg_disconnected = 'Disconnected'
 msg_booting = 'Booting...'
-msg_pgsparklite_ok = 'PGSparkLite\nVersion 1.1\n16/03/2021'
-msg_shutting_down = 'Shutting\ndown.\nPlease wait'
+msg_pgsparklite_ok = 'PGSparkLite\n\rVersion 2.0\n\r28/03/2021'
+msg_shutting_down = 'Shutting\n\rdown.\n\rPlease wait'
 msg_power_off = 'Safe to\npower off.'
